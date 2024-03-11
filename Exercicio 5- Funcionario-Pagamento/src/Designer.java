f5b84eee2b981f217f05a4528055099539a67ce7
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-03-11 15:22:03 -0300

    Criei uma nova Classe (Designer), e mudei algumas coisas na Main (Exibição dos Funcionários)

---
+++ Exercicio 5- Funcionario-Pagamento/src/Designer.java
@@ -0,0 +1,26 @@
+public class Designer extends Funcionario {
+
+    private int servicos;
+
+    Designer(String nome, int idade, int servicos){
+        super(nome, idade);
+        this.servicos = servicos;
+    }
+
+    public int getServicos(){
+        return servicos;
+    }
+
+    public void setServicos(){
+        this.servicos = servicos;
+    }
+
+    @Override
+    public double calcularSalario(){
+        return servicos * 350;
+    }
+
+
+
+
+}

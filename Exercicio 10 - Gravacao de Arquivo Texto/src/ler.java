e9cadb726dbdf455687fabcfc3fad7028d21e689
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-04-01 16:26:23 -0300

    Atualização - Exercicio) 10 ("Novo comentário no Código")

--- Exercicio 10 - Gravacao de Arquivo Texto/src/ler.java
+++ Exercicio 10 - Gravacao de Arquivo Texto/src/ler.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 public class ler {
     public static void lerArquivo(String nomeArquivo){
         try(BufferedReader reader = new BufferedReader(new FileReader(nomeArquivo))) {
+            //como foi passado por parâmetro, não vai precisar dar clear através do finally
+            // (ele dará automáticamente, pois seria como uma "variavel local")
             String linha;
             while ((linha = reader.readLine()) != null){
                 System.out.println(linha);
